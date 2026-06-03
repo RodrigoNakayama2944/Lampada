54ae31dc5f56eb0a2de540f6e2df998ed0fae33b
RodrigoNakayama2944 <rodrigo.nakayama@escola.pr.gov.br>
2026-06-03 11:08:30 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,31 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import ligado from "./assets/ligado.png"
 import desligado from "./assets/desligado.png"
+import logo from "./assets/LOGO.png"
 
 function App() {
   const [lampada, setLampada] = useState(desligado)
 
   return (
-   <div id='div-prin'>
-    <button type='button' onClick={() => setLampada(ligado)}>ligar</button>
-    <img src={lampada} id='lampada'/>
-    <button type='button' onClick={() => setLampada(desligado)}>desligar</button>
-   </div>
+  <div>
+    <div id="cabeçalho">
+      <img src={LOGO} id="logo"/>
+      <h3>Site para Lampadas grandes e saborosas</h3>
+      <h3>Feito por:</h3>
+      <ul>
+        <li>Rodrigo Nakama</li>
+        <li>Cristo Junior</li>
+        <li>Cristiano cézzáríónór</li>
+        <li>Nigolas Deellaattoorree</li>
+        <li>Predro Pedro Ricardo Ribeiro</li>
+      </ul> 
+    </div>
+    <div id='div-prin'>
+      <button type='button' onClick={() => setLampada(ligado)}>ligar</button>
+      <img src={lampada} id='lampada'/>
+      <button type='button' onClick={() => setLampada(desligado)}>desligar</button>
+    </div>
+  </div>
   )
 }
 
